825297884a1d4379cee72d93dab49bdadca71153
Pedro Argentati <pedro.argentatii@gmail.com>
2021-09-30 14:12:10 -0300

    Página responsiva - correções

--- js/script.js
+++ js/script.js
@@ -7,3 +7,27 @@ function activatedMenu() {
 
 buttonMobile.addEventListener('click', activatedMenu);
 buttonMobile.addEventListener('touchstart', activatedMenu);
+
+/* Máscaras ER */
+function mascara(o,f){
+    v_obj=o
+    v_fun=f
+    setTimeout("execmascara()",1)
+}
+function execmascara(){
+    v_obj.value=v_fun(v_obj.value)
+}
+function mtel(v){
+    v=v.replace(/\D/g,""); 
+    v=v.replace(/^(\d{2})(\d)/g,"($1) $2"); 
+    v=v.replace(/(\d)(\d{4})$/,"$1-$2"); 
+    return v;
+}
+function id( el ){
+	return document.getElementById( el );
+}
+window.onload = function(){
+	id('phone').onkeyup = function(){
+		mascara( this, mtel );
+	}
+}
